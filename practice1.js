5f46e94c99824830cef9f72867c69cb6d888ba91
fasteddiegarcia <fasteddiegarcia@gmail.com>
2015-02-28 20:09:58 -0600

    Edit 5
    Changed all photos to smaller file sizes,
    changed all copy to that supplied by client,

---
+++ practice1.js
@@ -0,0 +1,38 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);
+
+
+var compare = function (userChoice, computerChoice) {
+    
+if (userChoice === computerChoice) {
+    return "The result is a tie";
+}  
+
+else if(userChoice === "rock") {
+    
+    if(userChoice === "scissors") {
+    return "rock wins";
+    }
+     else {
+        return "paper wins";
+    }
+}
+
+if(userChoice === "paper") {
+    
+    if(userChoice === "rock") {
+    return "paper wins";
+    }
+     else {
+        return "scissors wins";
+    }
+}
+
+
